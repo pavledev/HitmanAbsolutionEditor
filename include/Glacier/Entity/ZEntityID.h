64089706960d337872ef7a989a9a607d75eaacf7
Pavle <pavle_nis@yahoo.com>
2023-12-12 12:32:36 +0100

    Remove SerializeToMemory and DeserializeFromJson functions from ZEntityID

--- include/Glacier/Entity/ZEntityID.h
+++ include/Glacier/Entity/ZEntityID.h
@@ -8,8 +8,6 @@ public:
 	ZEntityID() = default;
 	ZEntityID(const ZString& str);
 	void SerializeToJson(rapidjson::PrettyWriter<rapidjson::StringBuffer>& writer);
-	void SerializeToMemory(ZBinarySerializer& binarySerializer, const unsigned int offset);
-	static ZEntityID* DeserializeFromJson(const rapidjson::Value& object);
 
 private:
 	ZString m_sStr;
--- src/Glacier/Entity/ZEntityID.cpp
+++ src/Glacier/Entity/ZEntityID.cpp
@@ -14,19 +14,3 @@ void ZEntityID::SerializeToJson(rapidjson::PrettyWriter<rapidjson::StringBuffer>
 
 	writer.EndObject();
 }
-
-void ZEntityID::SerializeToMemory(ZBinarySerializer& binarySerializer, const unsigned int offset)
-{
-	unsigned int strOffset = offset + offsetof(ZEntityID, m_sStr);
-
-	m_sStr.SerializeToMemory(binarySerializer, strOffset);
-}
-
-ZEntityID* ZEntityID::DeserializeFromJson(const rapidjson::Value& object)
-{
-	ZEntityID* entityID = new ZEntityID();
-
-	entityID->m_sStr = *ZString::DeserializeFromJson(object["m_sStr"].GetString());
-
-	return entityID;
-}
