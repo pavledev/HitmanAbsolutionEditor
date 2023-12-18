fc65690cc01fceb366ccbe29b505283111a000b9
Pavle <pavle_nis@yahoo.com>
2023-12-18 09:28:51 +0100

    Add ChannelControl_PS.hlsl

---
+++ assets/shaders/ChannelControl_PS.hlsl
@@ -0,0 +1,45 @@
+cbuffer ChannelControlBuffer : register(b1)
+{
+    bool isRedChannelEnabled;
+	bool isGreenChannelEnabled;
+	bool isBlueChannelEnabled;
+	bool isAlphaChannelEnabled;
+};
+
+struct PS_INPUT
+{
+	float4 pos : SV_POSITION;
+    float4 col : COLOR0;
+    float2 uv  : TEXCOORD0;
+};
+
+sampler sampler0;
+Texture2D texture0;
+            
+float4 mainPS(PS_INPUT input) : SV_Target
+{
+	float4 outputColor = input.col * texture0.Sample(sampler0, input.uv);
+	
+	if (!isRedChannelEnabled)
+	{
+        outputColor.r = 0;
+    }
+	
+    if (!isGreenChannelEnabled)
+	{
+        outputColor.g = 0;
+    }
+	
+    if (!isBlueChannelEnabled)
+	{
+        outputColor.b = 0;
+    }
+	
+    if (!isAlphaChannelEnabled)
+	{
+        outputColor.a = 1;
+    }
+	
+	return outputColor;
+	//return float4(1.f, 0.f, 0.f, 1.f);
+}
