176b72273a1e1f009ee193cc5a0f6e647e347316
Pavle <pavle_nis@yahoo.com>
2023-12-20 11:16:10 +0100

    Add MultiLanguage class

---
+++ include/Resources/MultiLanguage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Resource.h"
+
+class MultiLanguage : public Resource
+{
+public:
+	void Parse();
+	char GetLocalizationCategory();
+	std::vector<int>& GetIndices();
+	std::vector<std::string>& GetLocales();
+
+private:
+	char localizationCategory;
+	std::vector<int> indices;
+	std::vector<std::string> locales;
+};
---
+++ src/Resources/MultiLanguage.cpp
@@ -0,0 +1,33 @@
+#include "Resources/MultiLanguage.h"
+
+void MultiLanguage::Parse()
+{
+	BinaryReader binaryReader = BinaryReader(GetResourceData(), GetResourceDataSize());
+	localizationCategory = binaryReader.Read<char>();
+	const unsigned int count = (GetResourceDataSize() - 1) >> 3;
+	const char* data = static_cast<const char*>(GetResourceData());
+
+	for (unsigned int i = 0; i < count; ++i)
+	{
+		const int index = *reinterpret_cast<const int*>(&data[8 * i + 5]);
+		const std::string locale = &data[8 * i + 1];
+
+		indices.push_back(index);
+		locales.push_back(locale);
+	}
+}
+
+char MultiLanguage::GetLocalizationCategory()
+{
+	return localizationCategory;
+}
+
+std::vector<int>& MultiLanguage::GetIndices()
+{
+	return indices;
+}
+
+std::vector<std::string>& MultiLanguage::GetLocales()
+{
+	return locales;
+}
