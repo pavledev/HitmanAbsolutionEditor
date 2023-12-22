d53b9ab308f1758529d9a0ee7266980a1693e136
Pavle <pavle_nis@yahoo.com>
2023-12-22 13:18:29 +0100

    Add WaveBankFSB class

---
+++ include/Resources/WaveBankFSB.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Resource.h"
+#include "FSB/FSB.h"
+
+class WaveBankFSB : public Resource
+{
+public:
+	struct AudioSample
+	{
+		~AudioSample();
+
+		std::string name;
+		void* data;
+		unsigned int dataSize;
+	};
+
+	const std::vector<std::shared_ptr<AudioSample>>& GetAudioSamples() const;
+	virtual FSB::Format GetFormat() = 0;
+	virtual void GetFSB5Data(void*& fsb5Data, unsigned int& fsb5Size) = 0;
+	void ConvertFSB5ToOGG();
+	void ConvertFSB5ToWAV();
+
+private:
+	std::vector<std::shared_ptr<AudioSample>> audioSamples;
+};
---
+++ src/Resources/WaveBankFSB.cpp
@@ -0,0 +1,71 @@
+#include "Resources/WaveBankFSB.h"
+#include "FSB/FSB.h"
+#include "FSB/Container.h"
+
+WaveBankFSB::AudioSample::~AudioSample()
+{
+	operator delete(data);
+}
+
+const std::vector<std::shared_ptr<WaveBankFSB::AudioSample>>& WaveBankFSB::GetAudioSamples() const
+{
+	return audioSamples;
+}
+
+void WaveBankFSB::ConvertFSB5ToOGG()
+{
+	void* fsb5Data = nullptr;
+	unsigned int fsb5Size = 0;
+
+	GetFSB5Data(fsb5Data, fsb5Size);
+
+	BinaryReader binaryReader = BinaryReader(fsb5Data, fsb5Size);
+	FSB::Container container = FSB::Container(binaryReader);
+
+	audioSamples.reserve(container.Samples().size());
+
+	for (FSB::Sample sample : container.Samples())
+	{
+		void* data = operator new(sample.size);
+		BinaryWriter binaryWriter = BinaryWriter(data, sample.size);
+
+		container.ExtractSample(sample, binaryWriter);
+
+		std::shared_ptr<AudioSample> audioSample = std::make_shared<AudioSample>();
+
+		audioSample->name = sample.name;
+		audioSample->data = binaryWriter.GetBuffer();
+		audioSample->dataSize = static_cast<unsigned int>(binaryWriter.GetPosition());
+
+		audioSamples.push_back(audioSample);
+	}
+}
+
+void WaveBankFSB::ConvertFSB5ToWAV()
+{
+	void* fsb5Data = nullptr;
+	unsigned int fsb5Size = 0;
+
+	GetFSB5Data(fsb5Data, fsb5Size);
+
+	BinaryReader binaryReader = BinaryReader(fsb5Data, fsb5Size);
+	FSB::Container container = FSB::Container(binaryReader);
+
+	audioSamples.reserve(container.Samples().size());
+
+	for (auto& sample : container.Samples())
+	{
+		void* data = operator new(sample.size);
+		BinaryWriter binaryWriter = BinaryWriter(data, sample.size);
+
+		container.ExtractSample(sample, binaryWriter);
+
+		std::shared_ptr<AudioSample> audioSample = std::make_shared<AudioSample>();
+
+		audioSample->name = sample.name;
+		audioSample->data = binaryWriter.GetBuffer();
+		audioSample->dataSize = static_cast<unsigned int>(binaryWriter.GetPosition());
+
+		audioSamples.push_back(audioSample);
+	}
+}
