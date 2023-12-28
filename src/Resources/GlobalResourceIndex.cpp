bb4050fb578ef8d214a70744d0e926feed35e596
Pavle <pavle_nis@yahoo.com>
2023-12-28 11:11:14 +0100

    Update GlobalResourceIndex.cpp

--- src/Resources/GlobalResourceIndex.cpp
+++ src/Resources/GlobalResourceIndex.cpp
@@ -1,46 +1,59 @@
-#include "Resources/GlobalResourceIndex.h"
 #include "Glacier/Serializer/ZBinaryDeserializer.h"
-#include "Logger.h"
 
-GlobalResourceIndex::~GlobalResourceIndex()
-{
-	operator delete(resourceIndex, std::align_val_t(alignment));
-}
+#include "Resources/GlobalResourceIndex.h"
+#include "Utility/ResourceUtility.h"
 
-SResourceIndex* GlobalResourceIndex::GetResourceIndex()
-{
-	return resourceIndex;
-}
-
-bool GlobalResourceIndex::Deserialize(const std::string& filePath)
-{
-	BinaryReader binaryReader = BinaryReader(filePath);
-
-	return Deserialize(binaryReader);
-}
-
-bool GlobalResourceIndex::Deserialize(void* data, const unsigned int dataSize)
-{
-	BinaryReader binaryReader = BinaryReader(data, dataSize);
-
-	return Deserialize(binaryReader);
-}
-
-bool GlobalResourceIndex::Deserialize(BinaryReader& binaryReader)
+void GlobalResourceIndex::Deserialize()
 {
 	ZBinaryDeserializer binaryDeserializer;
+	void* resourceIndex = binaryDeserializer.Deserialize(GetResourceData(), GetResourceDataSize());
 
-	resourceIndex = static_cast<SResourceIndex*>(binaryDeserializer.Deserialize(binaryReader));
-	alignment = binaryDeserializer.GetAlignment();
+	Parse(resourceIndex);
 
-	if (!resourceIndex)
+	operator delete(resourceIndex, std::align_val_t(binaryDeserializer.GetAlignment()));
+
+	isResourceDeserialized = true;
+}
+
+void GlobalResourceIndex::Parse(void* resourceIndex)
+{
+	this->resourceIndex = std::make_shared<SResourceIndex>();
+
+	const unsigned int foldersStartAddress = *reinterpret_cast<unsigned int*>(resourceIndex);
+	const unsigned int foldersEndAddress = *(reinterpret_cast<unsigned int*>(resourceIndex) + 1);
+	const unsigned int folderCount = ResourceUtility::CalculateArrayElementsCount(foldersStartAddress, foldersEndAddress, 0x14); //0x14 is size of SResourceIndexFolder
+
+	this->resourceIndex->folders.Resize(folderCount);
+
+	for (unsigned int i = 0; i < folderCount; i++)
 	{
-		Logger::GetInstance().Log(Logger::Level::Error, "Failed to deserialize global resource index!");
+		const unsigned int folderAddress = foldersStartAddress + 0x14 * i;
+		SResourceIndexFolder* resourceIndexFolder = ResourceUtility::Convert4ByteAddressTo8BytePointer<SResourceIndexFolder>(resourceIndex, folderAddress);
 
-		return false;
+		const unsigned int charsAddress = *(reinterpret_cast<unsigned int*>(resourceIndexFolder) + 1);
+		const char* chars = ResourceUtility::Convert4ByteAddressTo8BytePointer<const char>(resourceIndex, charsAddress);
+
+		this->resourceIndex->folders[i].name = ZString(chars);
+
+		const unsigned int resourceIndicesStartAddress = *(reinterpret_cast<unsigned int*>(resourceIndexFolder) + 2);
+		const unsigned int resourceIndicesEndAddress = *(reinterpret_cast<unsigned int*>(resourceIndexFolder) + 3);
+		const unsigned int resourceIndexCount = ResourceUtility::CalculateArrayElementsCount(resourceIndicesStartAddress, resourceIndicesEndAddress, sizeof(int));
+
+		this->resourceIndex->folders[i].resourceIndices.Resize(resourceIndexCount);
+
+		for (unsigned int j = 0; j < resourceIndexCount; j++)
+		{
+			const unsigned int resourceIndexAddress = resourceIndicesStartAddress + sizeof(int) * j;
+			int* resourceIndex2 = ResourceUtility::Convert4ByteAddressTo8BytePointer<int>(resourceIndex, resourceIndexAddress);
+
+			this->resourceIndex->folders[i].resourceIndices[j] = *resourceIndex2;
+		}
 	}
+}
 
-	return true;
+std::shared_ptr<SResourceIndex> GlobalResourceIndex::GetResourceIndex()
+{
+	return resourceIndex;
 }
 
 void GlobalResourceIndex::SerializeToJson(const std::string& outputFilePath)
