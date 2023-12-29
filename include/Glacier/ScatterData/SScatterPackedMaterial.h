116fd2d4ed2e014a5a6cdc8a1805f976d98db7e8
Pavle <pavle_nis@yahoo.com>
2023-12-29 08:49:38 +0100

    Create SScatterPackedMaterial.h

---
+++ include/Glacier/ScatterData/SScatterPackedMaterial.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../Resource/ZRuntimeResourceID.h"
+#include "SScatterPackedMaterialBase.h"
+
+struct SScatterPackedMaterial
+{
+	ZRuntimeResourceID m_idResource;
+	SScatterPackedMaterialBase m_Base;
+};
