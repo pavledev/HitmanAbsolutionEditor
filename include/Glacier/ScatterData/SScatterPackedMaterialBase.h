f4c0b53e23852fceca6792c9a70d47a595153cdf
Pavle <pavle_nis@yahoo.com>
2023-12-29 08:49:43 +0100

    Create SScatterPackedMaterialBase.h

---
+++ include/Glacier/ScatterData/SScatterPackedMaterialBase.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct SScatterPackedMaterialBase
+{
+	unsigned int m_nInstances;
+	unsigned int m_bBend;
+	float m_fBendConstraint;
+	float m_fCutoffDistance;
+	float m_fScaleBeginDistance;
+};
