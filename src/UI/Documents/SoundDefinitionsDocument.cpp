be15969ba26d5e0f5c3e6f09d0b7973a25a8586c
Pavle <pavle_nis@yahoo.com>
2023-12-29 18:50:51 +0100

    Create SoundDefinitionsDocument.cpp

---
+++ src/UI/Documents/SoundDefinitionsDocument.cpp
@@ -0,0 +1,76 @@
+#include <IconsMaterialDesignIcons.h>
+
+#include <UI/Documents/SoundDefinitionsDocument.h>
+#include <UI/Panels/SoundDefinitionsPanel.h>
+#include <UI/Panels/ResourceInfoPanel.h>
+#include <UI/Panels/HexViewerPanel.h>
+#include <Editor.h>
+
+SoundDefinitionsDocument::SoundDefinitionsDocument(const char* name, const char* icon, const Type type, const unsigned long long runtimeResourceID, const bool hasToolBar, const ImGuiID dockID) : Document(name, icon, type, runtimeResourceID, hasToolBar, dockID)
+{
+    soundDefinitions = std::make_shared<SoundDefinitions>();
+
+    std::shared_ptr<SoundDefinitionsPanel> soundDefinitionsPanel = std::make_shared<SoundDefinitionsPanel>("Sound Definitions", ICON_MDI_VOLUME_HIGH, soundDefinitions);
+    std::shared_ptr<ResourceInfoPanel> resourceInfoPanel = std::make_shared<ResourceInfoPanel>("Resource Info", ICON_MDI_INFORMATION, soundDefinitions);
+    std::shared_ptr<HexViewerPanel> headerLibraryHexViewerPanel = std::make_shared<HexViewerPanel>("Header Library Hex Viewer", ICON_MDI_MEMORY, false, soundDefinitions);
+    std::shared_ptr<HexViewerPanel> resourceLibraryhexViewerPanel = std::make_shared<HexViewerPanel>("Resource Library Hex Viewer", ICON_MDI_MEMORY, true, soundDefinitions);
+
+    AddPanel(soundDefinitionsPanel);
+    AddPanel(resourceInfoPanel);
+    AddPanel(headerLibraryHexViewerPanel);
+    AddPanel(resourceLibraryhexViewerPanel);
+
+    std::weak_ptr<SoundDefinitionsPanel> soundDefinitionsPanel2 = soundDefinitionsPanel;
+    std::weak_ptr<ResourceInfoPanel> resourceInfoPanel2 = resourceInfoPanel;
+
+    soundDefinitions->SetResourceLoadedCallback([soundDefinitionsPanel2, resourceInfoPanel2]()
+    {
+        std::shared_ptr<SoundDefinitionsPanel> soundDefinitionsPanel3 = soundDefinitionsPanel2.lock();
+        std::shared_ptr<ResourceInfoPanel> resourceInfoPanel3 = resourceInfoPanel2.lock();
+
+        if (soundDefinitionsPanel3)
+        {
+            soundDefinitionsPanel3->OnResourceLoaded();
+        }
+
+        if (resourceInfoPanel3)
+        {
+            resourceInfoPanel3->OnResourceLoaded();
+        }
+    });
+}
+
+void SoundDefinitionsDocument::CreateLayout(const ImGuiID dockspaceID, const ImVec2 dockspaceSize)
+{
+    ImGuiID mainDockID = dockspaceID;
+    ImGuiID rightDockID = ImGui::DockBuilderSplitNode(mainDockID, ImGuiDir_Right, 0.5f, nullptr, &mainDockID);
+
+    ImGui::DockBuilderDockWindow(CalculatePanelID(0, currentDockspaceID).c_str(), mainDockID);
+    ImGui::DockBuilderDockWindow(CalculatePanelID(1, currentDockspaceID).c_str(), rightDockID);
+    ImGui::DockBuilderDockWindow(CalculatePanelID(2, currentDockspaceID).c_str(), rightDockID);
+    ImGui::DockBuilderDockWindow(CalculatePanelID(3, currentDockspaceID).c_str(), rightDockID);
+    ImGui::DockBuilderFinish(dockspaceID);
+}
+
+std::shared_ptr<SoundDefinitions> SoundDefinitionsDocument::GetSoundDefinitions() const
+{
+    return soundDefinitions;
+}
+
+void SoundDefinitionsDocument::RenderMenuBar()
+{
+    if (!ImGui::BeginMenuBar())
+        return;
+
+    if (ImGui::BeginMenu("View"))
+    {
+        for (size_t i = 0; i < panels.size(); ++i)
+        {
+            ImGui::MenuItem(panels[i]->GetName(), nullptr, panels[i]->GetOpen());
+        }
+
+        ImGui::EndMenu();
+    }
+
+    ImGui::EndMenuBar();
+}
