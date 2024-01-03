2f0222ecdb4bac95cb1a2555a4fc96415b89accf
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:25 +0100

    Create CompositionType.cpp

---
+++ src/Resources/CompositionType.cpp
@@ -0,0 +1,9 @@
+#include "Resources/CompositionType.h"
+
+void CompositionType::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+}
