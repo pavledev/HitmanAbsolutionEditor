62dc6cdeaf96cdd4e264f9b6aa3f626f7825daa2
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:31 +0100

    Create EntityResource.cpp

---
+++ src/Resources/EntityResource.cpp
@@ -0,0 +1,9 @@
+#include "Resources/EntityResource.h"
+
+void EntityResource::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+}
