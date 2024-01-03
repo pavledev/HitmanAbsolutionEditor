38fa55a4f4970c2e82bec08ee051c8dcc57bfffc
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:17 +0100

    Create Animation.cpp

---
+++ src/Resources/Animation.cpp
@@ -0,0 +1,9 @@
+#include "Resources/Animation.h"
+
+void Animation::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+}
