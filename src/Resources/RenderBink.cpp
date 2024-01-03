f802387bcf4238aa898684231026c535cd234767
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:54 +0100

    Create RenderBink.cpp

---
+++ src/Resources/RenderBink.cpp
@@ -0,0 +1,16 @@
+#include "Resources/RenderBink.h"
+#include "IO/BinaryWriter.h"
+
+void RenderBink::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+	else
+	{
+		BinaryWriter binaryWriter = BinaryWriter(outputPath);
+
+		binaryWriter.Write(GetResourceData(), GetResourceDataSize());
+	}
+}
