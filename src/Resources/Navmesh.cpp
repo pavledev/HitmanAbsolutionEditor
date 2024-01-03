db188e8e620b4514ac3f7baeccd8018b3556045d
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:46 +0100

    Create Navmesh.cpp

---
+++ src/Resources/Navmesh.cpp
@@ -0,0 +1,9 @@
+#include "Resources/Navmesh.h"
+
+void Navmesh::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+}
