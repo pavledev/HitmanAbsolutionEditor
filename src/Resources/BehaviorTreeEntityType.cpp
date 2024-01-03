5e16c83bcea1bb8f0b87fe53bfa85bfc0d525190
Pavle <pavle_nis@yahoo.com>
2024-01-03 23:51:19 +0100

    Create BehaviorTreeEntityType.cpp

---
+++ src/Resources/BehaviorTreeEntityType.cpp
@@ -0,0 +1,9 @@
+#include "Resources/BehaviorTreeEntityType.h"
+
+void BehaviorTreeEntityType::Export(const std::string& outputPath, const std::string& exportOption)
+{
+	if (exportOption.starts_with("Raw"))
+	{
+		ExportRawData(outputPath);
+	}
+}
