82207d4adce58b4db239bc90185ae8b7da47a286
Pavle <pavle_nis@yahoo.com>
2024-01-03 08:34:21 +0100

    Add OpenSaveFileDialog function to FileDialog class

--- include/Utility/FileDialog.h
+++ include/Utility/FileDialog.h
@@ -6,4 +6,5 @@ class FileDialog
 {
 public:
 	static std::string OpenFolder();
+	static void OpenSaveFileDialog(const char* defaultFileName, const char* defaultExtension, const char* filters, std::string& outputFilePath, std::string& extension);
 };
--- src/Utility/FileDialog.cpp
+++ src/Utility/FileDialog.cpp
@@ -52,3 +52,30 @@ std::string FileDialog::OpenFolder()
 
     return folderPath;
 }
+
+void FileDialog::OpenSaveFileDialog(const char* defaultFileName, const char* defaultExtension, const char* filters, std::string& outputFilePath, std::string& extension)
+{
+    OPENFILENAME ofn;
+    char filePath[MAX_PATH] = "";
+    char fileTitle[MAX_PATH] = "";
+
+    strcpy_s(filePath, MAX_PATH, defaultFileName);
+
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL;
+    ofn.lpstrFilter = filters;
+    ofn.lpstrFile = filePath;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrFileTitle = fileTitle;
+    ofn.nMaxFileTitle = MAX_PATH;
+    ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+    ofn.lpstrDefExt = defaultExtension;
+
+    if (GetSaveFileName(&ofn))
+    {
+        outputFilePath = filePath;
+        extension = outputFilePath.substr(outputFilePath.find_last_of("."));
+    }
+}
