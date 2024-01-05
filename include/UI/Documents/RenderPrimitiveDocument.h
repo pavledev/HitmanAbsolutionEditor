a39941ab653ccbc7ed19ee8d1a153691ad6aabab
Pavle <pavle_nis@yahoo.com>
2024-01-05 10:26:36 +0100

    Update RenderPrimitiveDocument.h

--- include/UI/Documents/RenderPrimitiveDocument.h
+++ include/UI/Documents/RenderPrimitiveDocument.h
@@ -1,9 +1,18 @@
 #pragma once
 
 #include "Document.h"
+#include "Resources/RenderPrimitive.h"
 
 class RenderPrimitiveDocument : public Document
 {
 public:
-	RenderPrimitiveDocument();
+	RenderPrimitiveDocument(const char* name, const char* icon, const Type type, const unsigned long long runtimeResourceID, const bool hasToolBar, const ImGuiID dockID);
+	void CreateLayout(const ImGuiID dockspaceID, const ImVec2 dockspaceSize) override;
+	std::shared_ptr<RenderPrimitive> GetRenderPrimitive() const;
+	void RenderMenuBar() override;
+
+private:
+	void OnResourceLoaded();
+
+	std::shared_ptr<RenderPrimitive> renderPrimitive;
 };
