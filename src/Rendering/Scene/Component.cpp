c8ce99715cffde9eb711e04a52360d5e18123b7a
Pavle <pavle_nis@yahoo.com>
2024-01-12 18:12:04 +0100

    Update Component.cpp

--- src/Rendering/Scene/Component.cpp
+++ src/Rendering/Scene/Component.cpp
@@ -1,9 +1,6 @@
 #include "Rendering/Scene/Component.h"
-#include "Rendering/Scene/Model.h"
-#include "Rendering/Scene/Camera.h"
-#include "Rendering/Scene/PointLight.h"
 #include "Rendering/Scene/Transform.h"
-#include "Rendering/Scene/Grid.h"
+#include "Rendering/Scene/Entity.h"
 #include "Utility/StringUtility.h"
 
 Component::Component(const char* name, const char* icon, std::weak_ptr<Entity> entity) : entity(entity)
