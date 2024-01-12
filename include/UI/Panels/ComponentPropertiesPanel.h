feb9337f25c4cdbecbacd15cf8797ce550aa5b22
Pavle <pavle_nis@yahoo.com>
2024-01-12 18:09:31 +0100

    Update ComponentPropertiesPanel.h

--- include/UI/Panels/ComponentPropertiesPanel.h
+++ include/UI/Panels/ComponentPropertiesPanel.h
@@ -3,12 +3,16 @@
 #include "BasePanel.h"
 #include "Resources/RenderPrimitive.h"
 
+class Renderer3D;
+
 class ComponentPropertiesPanel : public BasePanel
 {
 public:
 	ComponentPropertiesPanel(const char* name, const char* icon, std::shared_ptr<RenderPrimitive> renderPrimitive);
 	void Render() override;
+	void SetRenderer3D(std::shared_ptr<Renderer3D> renderer3D);
 
 private:
 	std::shared_ptr<RenderPrimitive> renderPrimitive;
+	std::shared_ptr<Renderer3D> renderer3D;
 };
