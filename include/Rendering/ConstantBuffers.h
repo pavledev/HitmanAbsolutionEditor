1641c7b07b2eb7f93431cf8a290c6370faa070b9
Pavle <pavle_nis@yahoo.com>
2024-01-12 18:12:23 +0100

    Update ConstantBuffers.h

--- include/Rendering/ConstantBuffers.h
+++ include/Rendering/ConstantBuffers.h
@@ -1,58 +1,70 @@
 #pragma once
 
+#include <Windows.h>
+
 #include "Math/Matrix.h"
 #include "Math/Vector2.h"
 
-struct UberConstantBuffer
-{
-    Matrix model;
-    Matrix modelView;
-    Matrix modelViewProjection;
-    Matrix projection;
-    Matrix sphereMatrix;
+#define LIGHTS 8
 
-    float blurRadius = 5.0f;
-    float blurSigma = 0.0f;
+struct CameraConstantBuffer
+{
+    Matrix viewProjection;
+    Vector3 eyePosition; //Camera position
+    float padding;
+};
+
+struct MeshConstantBuffer
+{
+    Matrix world;
+    Matrix modelViewProjection;
+
+    BOOL hasDiffuseMap;
+    BOOL hasNormalMap;
+    BOOL hasSpecularMap;
+    BOOL hasEmissiveMap;
+    BOOL hasAlphaMap;
+    float vertexColorBlending = 0.f;
+    BOOL renderFlat = false; //Enable flat normal rendering
+    BOOL enableAutoTangent = false;
+
+    Vector4 materialDiffuse = 0.5f; //Kd := surface material's diffuse coefficient
+    Vector4 materialAmbient = 0.25f; //Ka := surface material's ambient coefficient.
+    Vector4 materialEmissive = 0.f; //Ke := surface material's emissive coefficient
+    Vector4 materialSpecular = 0.f; //Ks := surface material's specular coefficient.
+    Vector4 materialReflect = 0.f; //Kr := surface material's reflectivity coefficient.
+    float materialShininess = 1.f; //Ps := surface material's shininess
+    Vector3 padding;
+
+    float blurRadius;
+    float blurSigma;
     Vector2 blurDirection;
 
     Vector2 renderTargetResolution;
-    Vector2 padding;
+    Vector2 padding2;
 
     Vector4 outlineColor;
 
-    Vector3 specularColor;
-    float padding2;
-
     Vector3 materialColor;
     float padding3;
-
-    bool useGlossAlpha;
-    bool useSpecularMap;
-    bool useNormalMap;
-    float padding4;
-
-    float specularWeight;
-    float specularGloss;
-    float normalMapWeight;
-    float padding5;
-    Vector2 padding6;
 };
 
-struct PointLightConstantBuffer
+struct Light
 {
-    Matrix transform;
+    int lightType;
+    Vector3 padding;
+    //The light direction is here the vector which looks towards the light
+    Vector4 lightDirection;
+    Vector4 lightPosition;
+    Vector4 lightAttenuation;
+    Vector4 lightSpot; //(outer angle, inner angle, falloff, free)
+    Vector4 lightColor;
+};
 
-    Vector3 viewLightPos;
-	float padding;
-
-    Vector3 ambientColor;
-	float padding2;
-
-    Vector3 diffuseColor;
-	float padding3;
-
-	float diffuseIntensity;
-	float attConst;
-	float attLin;
-	float attQuad;
+struct LightConstantBuffer
+{
+    Light lights[LIGHTS];
+    Vector4 lightAmbient = Vector4(0.2f, 0.2f, 0.2f, 1.0f);
+    int lightCount;
+    Vector3 padding;
 };
